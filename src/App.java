5e2e72a06dd3911dea3deeb761ca9a7ffdbd0fba
unknown <natashachipeta@gmail.com>
2023-12-21 01:17:22 +0200

    imnplementing the number replacement logic

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner userInput = new Scanner(System.in);
+
+        // getting the first number of the range
+        System.out.print("Enter the start of the range: ");
+        int startRange = userInput.nextInt();
+        // getting the last number of the range
+        System.out.print("Enter the end of the range: ");
+        int endRange = userInput.nextInt();
+
+        // looping through the range using a for-loop
+        for(int i = startRange; i <= endRange; i++){
+            // checking the numbers in the range
+            if(i % 3 == 0){
+                System.out.println("ray");
+            } else if (i % 5 == 0){
+                System.out.println("roy");
+            } else {
+                System.out.println(i);
+            }
+        }
+        userInput.close();
     }
 }
