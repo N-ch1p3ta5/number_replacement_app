ed3dbba391123b41c10629b045c876bb0856335a
unknown <natashachipeta@gmail.com>
2023-12-21 01:38:39 +0200

    adding the condition to check if number is divisble by both 3 and 5

--- src/App.java
+++ src/App.java
@@ -14,10 +14,12 @@ public class App {
         // looping through the range using a for-loop
         for(int i = startRange; i <= endRange; i++){
             // checking the numbers in the range
-            if(i % 3 == 0){
-                System.out.println("ray");
+            if((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("ray-roy");
             } else if (i % 5 == 0){
                 System.out.println("roy");
+            } else if(i % 3 == 0){
+                System.out.println("ray");
             } else {
                 System.out.println(i);
             }
